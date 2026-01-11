ec4404bb27fed7d72eb77d913f12011890176abb
KRothfus <krothfus92@gmail.com>
2026-01-11 13:00:03 -0800

    ON to CH2!
    learning about cacheing.

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,11 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+import { Buffer } from "buffer";
+import path from "path";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -53,19 +24,36 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   if (!(file instanceof File)) {
     throw new BadRequestError("No thumbnail file provided");
   }
-  
-  if (file.size > 10 << 20) { // 10MB limit
+  if (
+    file.type.split("/")[1].includes("jpeg") ||
+    file.type.split("/")[1].includes("png")
+  ) {}
+  else {
+    throw new BadRequestError(
+      `Invalid thumbnail file type. Only JPEG and PNG are allowed. ${
+        file.type.split("/")[1]
+      }`
+    );
+  }
+  if (file.size > 10 << 20) {
+    // 10MB limit
     throw new BadRequestError("Thumbnail file size exceeds 10MB limit");
   }
   const mediaType = file.type;
-  const imageData = await file.arrayBuffer();
+  const imageData = Buffer.from(await file.arrayBuffer());
   const metadata = getVideo(cfg.db, videoId);
   if (metadata?.userID !== userID) {
-    throw new UserForbiddenError("You do not have permission to upload a thumbnail for this video");
+    throw new UserForbiddenError(
+      "You do not have permission to upload a thumbnail for this video"
+    );
   }
-  videoThumbnails.set(videoId,{data: imageData, mediaType: mediaType});
 
-  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/:${videoId}`;
-  
-  return respondWithJSON(200, null);
+  const fileh = `/assets/${videoId}.${file.type.split("/")[1]}`;
+  const pathURL = path.join(cfg.assetsRoot, fileh);
+  await Bun.write(pathURL, imageData);
+  // const dataURL = `data:image/png;base64,${imageData}`;
+  const thumbnailURL = `http://localhost:${cfg.port}` + fileh;
+  metadata.thumbnailURL = thumbnailURL;
+  updateVideo(cfg.db, metadata);
+  return respondWithJSON(200, { metadata });
 }
